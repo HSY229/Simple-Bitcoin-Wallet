398e3c8a11c9e75241881842d5758ee63f82bd65
Yen <hsy1988229@gmail.com>
2018-05-29 11:50:49 +0800

    uses toolbar to display the balance
    not push up the whole view when Snackbar is displayed.

--- app/src/main/java/com/hsy/simplebitcoinwallet/main/MainFragment.java
+++ app/src/main/java/com/hsy/simplebitcoinwallet/main/MainFragment.java
@@ -1,7 +1,9 @@
 package com.hsy.simplebitcoinwallet.main;
 
 import android.app.Activity;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,23 +25,32 @@ import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Use the {@link #newInstance()} factory method to create an instance of this fragment.
  */
 public class MainFragment extends BaseView<MainViewModel, FragmentMainBinding> {
 
   private TxListAdapter txListAdapter;
 
+  /**
+   * Call {@link #newInstance()} to create this fragment, and then give it {@link MainViewModel}.
+   * Finally, add it to activity.
+   */
   public static void addToActivity(@NonNull FragmentManager fragmentManager, @NonNull BtcWalletManager btcWalletManager) {
     final MainFragment view = newInstance();
     view.setViewModel(new MainViewModel(btcWalletManager));
     ActivityUtils.addFragmentToActivity(fragmentManager, view, R.id.contentFrame);
   }
 
+  /**
+   * Default and empty constructor.
+   */
   public MainFragment() {
     // Required empty public constructor
   }
 
+  /**
+   * Create and initialize a new instance of this class.
+   */
   @NonNull
   public static MainFragment newInstance() {
     return new MainFragment();
@@ -47,8 +59,7 @@ public class MainFragment extends BaseView<MainViewModel, FragmentMainBinding> {
   @NonNull
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    binding = DataBindingUtil
-        .inflate(inflater, R.layout.fragment_main, container, false);
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
     binding.setViewModel(viewModel);
     return binding.getRoot();
   }
@@ -56,21 +67,31 @@ public class MainFragment extends BaseView<MainViewModel, FragmentMainBinding> {
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
+    setUpActionBar();
     setupListAdapter();
     setupRefreshLayout();
-    ViewCompat.setNestedScrollingEnabled(binding.txsContainer, false);
     txListAdapter.notifyDataSetChanged();
     if (viewModel != null) {
       viewModel.start(binding.txsContainer.getAdapter());
     }
   }
 
+  private void setUpActionBar() {
+    final AppCompatActivity appCompatActivity = ((AppCompatActivity) getActivity());
+    if (appCompatActivity != null) {
+      appCompatActivity.setSupportActionBar(binding.toolbar);
+    }
+    binding.toolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
+    binding.toolbarLayout.setExpandedTitleColor(Color.YELLOW);
+  }
+
   private void setupListAdapter() {
-    binding.txsContainer.setLayoutManager(new LinearLayoutManager(getContext()));
-    txListAdapter = new TxListAdapter(
-        getContext(),
-        new ArrayList<>(0));
-    binding.txsContainer.setAdapter(txListAdapter);
+    final Context context = getContext();
+    if (context != null) {
+      binding.txsContainer.setLayoutManager(new LinearLayoutManager(context));
+      txListAdapter = new TxListAdapter(context, new ArrayList<>(0));
+      binding.txsContainer.setAdapter(txListAdapter);
+    }
 
     ViewCompat.setNestedScrollingEnabled(binding.txsContainer, false);
   }
